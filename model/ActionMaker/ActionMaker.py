f67a6836269af901a5f0cfc2eeeb5ff7241d6914
Sergey Sokolnik <sokolniksergey@gmail.com>
2021-03-28 20:48:26 +0300

    feat(model): exclude duplicated sendings(post to group)

--- model/ActionMaker/ActionMaker.py
+++ model/ActionMaker/ActionMaker.py
@@ -1,6 +1,6 @@
 from model.Enums.ActionsEnum import ActionsEnum
 from model.Containers.ActionQueue.Action import Action
-import random
+import random, hashlib
 
 class ActionMaker:
 
@@ -21,6 +21,7 @@ class ActionMaker:
     def __init__(self,action_queue,t_o_send_post,t_o_get_group,t_o_internal_operations):
 
         self.__action_queue = action_queue
+        self.__scheduled_actions_hashes = []
 
         ActionMaker.TIME_OUT_SEND_POST = t_o_send_post
 
@@ -28,18 +29,30 @@ class ActionMaker:
 
         ActionMaker.TIME_OUT_INTERNAL_OPERATIONS  = t_o_internal_operations
 
+    def create_send_posts_to_groups_actions(self, groups, posts):
+        for pair in self.get_group_and_post_to_send(groups, posts):
+            group, post = pair
 
-    def create_send_posts_to_groups_actions(self,groups,posts):
+            post_to_group_string = str(group.gid) + post.text
+            post_to_group_hash = hashlib.md5(post_to_group_string.encode('utf-8')).hexdigest()
+
+            if post_to_group_hash not in self.__scheduled_actions_hashes: # to exclude duplicated sendings during one start
+                self.__action_queue.add_action_to_leisurely_queue(
+                    self.create_send_post_to_group_action(group.gid, post.text, post.list_of_photos))
+            self.__scheduled_actions_hashes.append(post_to_group_hash)
+
+    def get_group_and_post_to_send(self, groups, posts):
+        pairs_to_send = []
         if ActionMaker.EACH_POST_TO_EACH_GROUP:
             for post in posts:
                 for group in groups:
-                    self.__action_queue.add_action_to_leisurely_queue(
-                        self.create_send_post_to_group_action(group.gid, post.text, post.list_of_photos))
+                    pairs_to_send.append([group, post])
         else:
             for group in groups:
                 rand_post = random.choice(posts)
-                self.__action_queue.add_action_to_leisurely_queue(
-                  self.create_send_post_to_group_action(group.gid,rand_post.text,rand_post.list_of_photos))
+                pairs_to_send.append([group, rand_post])
+
+        return  pairs_to_send
 
     def create_search_groups_action(self,key_word) :
         self.__action_queue.add_action_to_hurriedly_queue(
