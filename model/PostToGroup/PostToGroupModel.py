f088fe2a5e24543d84a30d6d8b3471a50d2a3ecd
Sergey Sokolnik <sokolniksergey@gmail.com>
2021-06-08 10:04:09 +0300

    minor label changes

--- model/PostToGroup/PostToGroupModel.py
+++ model/PostToGroup/PostToGroupModel.py
@@ -122,7 +122,7 @@ class PostToGroupModel(QObject):
         timestamps = []
         for sending in resendings:
             timestamps.append(int(sending['timestamp']))
-        label_to_show = f"Resend {len(resendings)} for the current post (last at {datetime.fromtimestamp(max(timestamps)).date()})"
+        label_to_show = f"Resend {len(resendings)} for the current post (last on {datetime.fromtimestamp(max(timestamps)).date()})"
         return label_to_show
 
     def append_resend_by_post(self, post_text):
